d0e46b77af58fd891909187d353fd8e0eefc3ee8
Yggdrasil09 <karthik99th.tnk@gamil.com>
2018-12-25 23:10:42 +0530

    Imported the required module and window for the game

---
+++ pong.py
@@ -0,0 +1,15 @@
+# A ping pong game built by Karthik
+# using turtle module
+
+import turtle
+# a basic graphic library
+
+win = turtle.Screen()
+win.title("A PING-PONG Game")
+win.bgcolor('black')
+win.setup(width=800,height=600)
+win.tracer(0)
+
+
+while True:
+    win.update()
